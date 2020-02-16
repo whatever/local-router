35a47af24065f0ceb1cedb77b6c8b576d9f591b9
whatever <mattisbusiness@gmail.com>
2020-02-16 09:41:32 -0500

    :seedling:

---
+++ router.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var version string = "?"
+
+var Map map[string]int = map[string]int{
+	"whatever.org":       9000,
+	"nevermind.business": 9001,
+	"anyway.net":         9002,
+}
+
+// PortMap returns the map
+func PortMap(url string) (port int) {
+	var ok bool
+	if port, ok = Map[url]; !ok {
+		port = 9999
+	}
+	return
+}
+
+// ProxyGet proxies a get request
+func ProxyGet(r *http.Request) (resp *http.Response, err error) {
+	port := PortMap(r.Host)
+	url := fmt.Sprintf("http://0.0.0.0:%v", port)
+	resp, err = http.Get(url)
+	return resp, err
+}
+
+func main() {
+
+	port := flag.Int("port", 8080, "listen on port")
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if resp, err := ProxyGet(r); err != nil {
+			fmt.Println(err)
+		} else if body, err := ioutil.ReadAll(resp.Body); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Fprintf(w, string(body))
+		}
+	})
+
+	fmt.Printf("Router v%v\n", version)
+	fmt.Printf("Port ....... %v\n", *port)
+	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%v", *port), nil))
+}
