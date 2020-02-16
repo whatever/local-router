d8b19625686608ec551d75e5852fff94a00cc788
whatever <mattisbusiness@gmail.com>
2020-02-16 13:38:46 -0500

    Read %v: %d lines from config file

--- router.go
+++ router.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 )
 
 var version string = "?"
 
-var Map map[string]int = map[string]int{
-	"whatever.org":       9000,
-	"nevermind.business": 9001,
-	"anyway.net":         9002,
-}
+var Map map[string]int = map[string]int{}
 
 // PortMap returns the map
 func PortMap(url string) (port int) {
@@ -38,11 +38,56 @@ func ProxyRequest(r *http.Request) (resp *http.Response, err error) {
 	return resp, err
 }
 
+func LoadConfig(file_name string) {
+	fi, err := os.Open(file_name)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(2)
+		return
+	}
+
+	scanner := bufio.NewScanner(fi)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			fmt.Println("Skipping empty line")
+			continue
+		}
+
+		pieces := strings.Split(line, ":")
+
+		if len(pieces) != 2 {
+			fmt.Println("Skipping invalid line:", line)
+			continue
+		}
+
+		pieces[0] = strings.TrimSpace(pieces[0])
+		pieces[1] = strings.TrimSpace(pieces[1])
+
+		if pieces[0] == "" {
+			fmt.Println("Skipping empty hostname")
+			continue
+		}
+
+		if n, e := strconv.Atoi(pieces[1]); e == nil {
+			Map[pieces[0]] = n
+		}
+	}
+}
+
 func main() {
 
 	port := flag.Int("port", 8080, "listen on port")
+	config := flag.String("config", "", "")
 	flag.Parse()
 
+	// This mutates the map!
+	LoadConfig(*config)
+
+	// Single proxy handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if resp, err := ProxyRequest(r); err != nil {
 			fmt.Println(err)
@@ -58,5 +103,8 @@ func main() {
 
 	fmt.Printf("Router v%v\n", version)
 	fmt.Printf("Port ....... %v\n", *port)
+	for host, port := range Map {
+		fmt.Printf("%v -> %v\n", host, port)
+	}
 	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%v", *port), nil))
 }
